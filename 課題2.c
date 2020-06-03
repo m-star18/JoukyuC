3231dbbc3e5ef05cfdd38849584a70decf3e71fa
m-star18 <ryuuchei@ezweb.ne.jp>
2020-06-03 22:06:29 +0900

    課題２　完了

---
+++ 課題2.c
@@ -0,0 +1,26 @@
+a
+#include <stdio.h>
+#include <math.h>
+#define N 5
+
+struct node {
+    double x;
+    double y;
+    double dist;
+};
+
+int main(void) {
+    struct node xy[N];
+    int i;
+
+    for (i = 0; i < N; i++) {
+        printf("point[%d] (x, y) = ", i);
+        scanf("%lf %lf", &xy[i].x, &xy[i].y);
+    }
+
+    for (i = 0; i < N - 1; i++) {
+        xy[i].dist = sqrt(pow(xy[i].x - xy[i + 1].x, 2.0) + pow(xy[i].y - xy[i + 1].y, 2.0));
+        printf("Location(%.2lf, %.2lf) - (%.2lf, %.2lf), ", xy[i].x, xy[i].y, xy[i + 1].x, xy[i + 1].y);
+        printf("dist = %.2lf\n", xy[i].dist);
+    }
+}
