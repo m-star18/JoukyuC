a1c3ad7f26fc620fe287081f981734fcffadf17a
m-star18 <ryuuchei@ezweb.ne.jp>
2020-07-20 02:20:12 +0900

    Complete 課題5

---
+++ 前期/課題/課題5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 1000000
+#define CLOCK_PER_SEC 1000
+
+int compare_int(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
+}
+
+int binary_search(const int data[], int check) {
+    int low = 0;
+    int high = N - 1;
+    int mid;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (data[mid] == check)
+            return mid;
+
+        else if (data[mid] > check)
+            high = mid - 1;
+
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
+
+int main() {
+    int data[N], ans;
+    int sort_time, search_time;
+    int m, k;
+
+    scanf("%d %d", &m, &k);
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < N; i++)
+        data[i] = rand() % m;
+
+    sort_time = clock();
+    qsort(data, N, sizeof(int), compare_int);
+    sort_time -= clock();
+
+    search_time = clock();
+    ans = binary_search(data, k);
+    search_time -= clock();
+
+    printf("２分探索　結果: %d\n", ans);
+    printf("計測時間 ソート: %f[ms]\n", -(double)sort_time / CLOCK_PER_SEC);
+    printf("計測時間 ２分探索: %f[ms]\n", -(double)search_time / CLOCK_PER_SEC);
+}
