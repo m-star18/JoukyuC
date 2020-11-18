ad48cbf369c9325b0fcadbd234f6d8de5e7fa687
m-star18 <ryuuchei@ezweb.ne.jp>
2020-11-18 10:20:43 +0900

    Update ex13.c

--- 後期/演習/ex13/ex13.c
+++ 後期/演習/ex13/ex13.c
@@ -1,11 +1,4 @@
-﻿/* 上級CプログラミングB 出題日：2020/11/16
-   3I ○番 氏名 提出日：忘れずに記入！！ */
-/* ファイル名：ex13.c
-   演習 線形リストによるスタック，キューの実現
-   設問：pop(), push(), enterq(), removeq() の作成
-*/
-
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 
 struct Element {
@@ -14,65 +7,109 @@ struct Element {
 };
 struct Element *head, *tail;
 
-void initialize()
-{
-    head = tail = NULL;
+void initialize() {
+    head = NULL;
+    tail = NULL;
 }
 
 /* 設問 １：スタックの入出力関数 push, pop を作成 */
-void push(int data)
-{
+void push(int data) {
     struct Element *p;
-    p = ;
-    p->data =;
-    p->next =;
-    head =;
+
+    p = malloc(sizeof(struct Element));
+    p->data = data;
+    p->next = head;
+    head = p;
 }
 
-int pop()
-{
+int pop() {
+    if (head == NULL)
+        return -1;
+
+    else {
+        int data;
+        struct Element *p;
+
+        p = head;
+        data = p->data;
+        head = p->next;
+        free(p);
+        return data;
+    }
 }
 
 /* 設問 2：キューの入出力関数 enterq, removeq を作成 */
-void enterq (int data)
-{
+void enterq (int data) {
+    struct Element *p;
+
+    p = malloc(sizeof(struct Element));
+    p->data = data;
+
+    if (head == NULL) {
+        head = p;
+        tail = p;
+
+    } else {
+        tail->next = p;
+        tail = p;
+    }
+    tail->next = NULL;
 }
 
-int removeq()
-{
+int removeq() {
+    if (head == NULL)
+        return -1;
 
-void display() /* 変更しない */
-{
+    else {
+        int data;
+        struct Element *p;
+
+        p = head;
+        data = p->data;
+        head = p->next;
+        free(p);
+        return data;
+    }
+}
+
+void display() {
     int i;
     struct Element *p;
-    for (i=0,p=head; p!=NULL; i++,p=p->next)
+
+    for (i = 0, p = head; p != NULL; i++, p = p->next)
         printf("%6d: %p %6d %p\n", i, p, p->data, p->next);
 }
 
-/* スタックを使用する場合には push(id);, id=pop(); の行，
-   キューを使用する場合には enterq(id);, id=removeq(); の行
-   のコメントを外す
- */
-int main(void)
-{
-    int mode = 1, id;
+int main(void) {
+    int mode = 1, id, flag;
+
+    printf("stack: 1 queue: 2 ");
+    scanf("%d", &flag);
+
     initialize();
     while (mode) {
         printf("list process ?input(1) or output(2) = ");
         scanf("%d", &mode);
-        if(mode == 1){
+
+        if (mode == 1) {
             printf("?id = ");
             scanf("%d", &id);
-            /* push(id); //スタック */
-            /* enterq(id); //キュー */
-        }else if(mode == 2){
-            /* id = pop(); //スタック */
-            /* id = removeq(); //キュー */
-            if(id == -1){  // 取り出し失敗
+
+            if (flag == 1)
+                push(id);
+            else
+                enterq(id);
+
+        } else if (mode == 2) {
+            if (flag == 1)
+                id = pop();
+            else
+                id = removeq();
+
+            if (id == -1)  // 取り出し失敗
                 printf("empty\n");
-            }else{
+            else
                 printf("id = %d was picked\n", id);
-            }
         }
 
         display();
@@ -80,5 +117,3 @@ int main(void)
         scanf("%d", &mode);
     }
 }
-
-/* スタック，キュー の動作が確認ができる出力結果を貼る */
