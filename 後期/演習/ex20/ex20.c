3ac6f073fbb9c0cdfa965750fe2e6d73564b2149
m-star18 <ryuuchei@ezweb.ne.jp>
2020-12-17 00:34:55 +0900

    Update ex20.c

--- 後期/演習/ex20/ex20.c
+++ 後期/演習/ex20/ex20.c
@@ -1,10 +1,4 @@
-﻿/* 上級CプログラミングB 出題日：2020/12/16
-   3I 番 氏名 提出日：忘れずに記入！！ */
-/* ファイル名：ex20.c
-   リストによる自己再編成探索
-*/
-
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -14,49 +8,67 @@ struct Element {
 };
 struct Element *head;
 
-void initialize()
-{
-    head = NULL;
+void add(char data[]) {
+    struct Element *p;
+
+    p = (struct Element *) malloc(sizeof(struct Element));
+    strcpy(p->data, data);
+    p->next = head;
+    head = p;
 }
 
-void display()
-{
+void initialize() {
+    head = NULL;
+
+    add("興");
+    add("鋼");
+    add("公");
+    add("校");
+    add("巧");
+    add("晃");
+    add("皇");
+    add("項");
+    add("高");
+    add("工");
+}
+
+void display() {
     int i;
     struct Element *p;
-    for (i=0,p=head; p!=NULL; i++,p=p->next)
+
+    for (i = 0, p = head; p != NULL; i++, p = p->next)
         printf("%3d: %s", i, p->data);
     printf("\n");
 }
 
-int main(void)
-{
-    initialize();
-    /* 設問：探索用リストを構築
-        "興", "鋼", "公", "校", "巧", "晃", "皇", "項", "高", "工"
-    */
-
-    /* 以下で自己再編成探索システムを記述 */
+int main(void) {
     int mode = 1, id;
 
+    /* 設問：探索用リストを構築 "興", "鋼", "公", "校", "巧", "晃", "皇", "項", "高", "工" */
+    initialize();
+
     display();
-    while(mode) {
+    while (mode) {
         int i;
         struct Element *p, *q;
 
         printf("?id = ");
         // 設問：id の読み取り(この id つまり番号が変換したい漢字を指すことになる)
-        scanf();
+        scanf("%d", &id);
 
         /* 設問：自己再編成探索 */
-        // p を 選択した候補，q をその一つ前まで移動させる（id 番目まで移動させる）
-        for ();
+        for (i = 0, p = q = head; i < id && p->next != NULL; i++, q = p, p = p->next);
+
         // 設問：変換結果（選択した候補のデータ部）の表示
-        printf();
-        if (){ // 選択した候補が既に先頭か，リストが空のとき，何もしない
-        } else {
-          // 設問：自己再編成探索．p と q の next, head をつなぎかえ
+        printf("%s\n", p->data);
+        if (p != head && p != NULL) {
+            // 設問：自己再編成探索．p と q の next, head をつなぎかえ
+            q->next = p->next;
+            p->next = head;
+            head = p;
         }
         display();
-        printf("?continue(1) or quit(0) = "); scanf("%d", &mode);
+        printf("?continue(1) or quit(0) = ");
+        scanf("%d", &mode);
     }
 }
